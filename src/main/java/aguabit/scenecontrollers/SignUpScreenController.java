abc7d37fe90f6a3175d4a86be0622fecdf487ecd
Nrojt <tjornbrederoo@gmail.com>
2022-12-05 12:57:48 +0100

    A new account can be created, checks still need to be added

--- src/main/java/aguabit/scenecontrollers/SignUpScreenController.java
+++ src/main/java/aguabit/scenecontrollers/SignUpScreenController.java
@@ -8,12 +8,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ResourceBundle;
 
-public class SignUpScreenController implements Initializable {
+public class SignUpScreenController{
     //making variables for the textfields and the button on this screen
     @FXML
     public TextField usernameTextfield = new TextField();
@@ -32,6 +30,7 @@ public class SignUpScreenController implements Initializable {
     @FXML
     public MFXButton signupButton = new MFXButton();
 
+    protected String querry = "INSERT INTO user(first_name, last_name, username, phonenumber, password, email, date_of_birth) VALUES(?,?,?,?,?,?,?)";;
 
 
     public void printInput(ActionEvent e){
@@ -43,6 +42,23 @@ public class SignUpScreenController implements Initializable {
             System.out.println("email " + emailTextfield.getText());
             System.out.println("password " + passwordTextfield.getText());
             System.out.println("date of birth " + dobPicker.getValue());
+            DatabaseConnection connectionNow = new DatabaseConnection();
+            //Connection connectDB = connectionNow.getDBConnection();
+            try (Connection connectDB = connectionNow.getDBConnection();
+                 PreparedStatement pstmt = connectDB.prepareStatement(querry)) {
+                pstmt.setString(1, firstnameTextfield.getText());
+                pstmt.setString(2, lastnameTextfield.getText());
+                pstmt.setString(3, usernameTextfield.getText());
+                pstmt.setInt(4, Integer.parseInt(phonenumberTextfield.getText()));
+                pstmt.setString(5, passwordTextfield.getText());
+                pstmt.setString(6, emailTextfield.getText());
+                pstmt.setDate(7, Date.valueOf(dobPicker.getValue()));
+
+                pstmt.executeUpdate();
+            } catch (SQLException z) {
+                System.out.println(z.getMessage());
+            }
+
         } else{
             System.out.println("not filled");
         }
@@ -50,27 +66,4 @@ public class SignUpScreenController implements Initializable {
     }
 
 
-
-    public void initialize(URL location, ResourceBundle resources) {
-        DatabaseConnection connectionNow = new DatabaseConnection();
-        Connection connectDB = connectionNow.getDBConnection();
-
-        String connectQuery = "SELECT * FROM user";
-
-        try {
-
-            Statement statement = connectDB.createStatement();
-            ResultSet queryOutput = statement.executeQuery(connectQuery);
-
-            while (queryOutput.next()) {
-
-                String user_id = queryOutput.getString("user_id");
-                String first_name = queryOutput.getString("first_name");
-                String username = queryOutput.getString("username");
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
