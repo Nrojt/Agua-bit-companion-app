2e7cbde5a95777a87cda7213f161353f744dcf4e
Nrojt <tjornbrederoo@gmail.com>
2022-12-14 14:41:37 +0100

    -darkmode can now be activated by setting it to true in settings.text, needs a button on the settings screen

--- src/main/java/aguabit/scenecontrollers/Main.java
+++ src/main/java/aguabit/scenecontrollers/Main.java
@@ -6,20 +6,30 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import saveFile.SaveFile;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class Main extends Application {
-    private String csstheme = "lightmode.css";
+    static Scene scene;
+    private  String lightTheme = "lightmode.css";
+    private String darkTheme = "darkmode.css";
+    private String cssfile;
     //override gets run after loading in
     @Override
     public void start(Stage stage) throws IOException {
+        SaveFile.readSettingsFromFile();
+        if(SaveFile.darkmode == true){
+            cssfile = darkTheme;
+        }else{
+            cssfile = lightTheme;
+        }
         //code for loading in the fxml and css file for running the application.
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MenuOverlay.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),1280,720);
+        scene = new Scene(fxmlLoader.load(),1280,720);
         stage.setTitle("Agua:bit companion app");
-        String css = Objects.requireNonNull(this.getClass().getResource(csstheme)).toExternalForm();
+        String css = Objects.requireNonNull(this.getClass().getResource(cssfile)).toExternalForm();
         stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("logo.png"))));
         scene.getStylesheets().add(css);
         stage.setScene(scene);
@@ -45,7 +55,6 @@ public class Main extends Application {
         stage.close();
         Platform.exit();
     }
-
 
     //this actually runs the program
     public static void main(String[] args) {
--- src/main/java/aguabit/scenecontrollers/MainScreenController.java
+++ src/main/java/aguabit/scenecontrollers/MainScreenController.java
@@ -4,19 +4,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.stage.Stage;
-import java.io.IOException;
+
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
-public class MainScreenController implements Initializable{
+public class MainScreenController implements Initializable {
     @FXML
     private Label userNameLabel;
 
     public MainScreenController() {
         //okay so this gets run every time a screen loads
     }
-
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
