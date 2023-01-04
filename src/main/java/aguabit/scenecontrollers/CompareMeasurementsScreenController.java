64a87bdefa2b2e38f7af028386bc4f9731137be8
Nrojt <tjornbrederoo@gmail.com>
2023-01-04 16:33:32 +0100

    -Added a back button to the CompareMeasurementsScreen

--- src/main/java/aguabit/scenecontrollers/CompareMeasurementsScreenController.java
+++ src/main/java/aguabit/scenecontrollers/CompareMeasurementsScreenController.java
@@ -1,13 +1,24 @@
 package aguabit.scenecontrollers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import saveFile.SaveFile;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class CompareMeasurementsScreenController implements Initializable {
+    @FXML
+    private AnchorPane fxmlpane = new AnchorPane();
     //Declaring the labels, M1 is measurement 1, M2 is measurement 2
     @FXML
     private Label m1Slot1TypeLabel = new Label();
@@ -115,5 +126,57 @@ public class CompareMeasurementsScreenController implements Initializable {
         m2DateLabel.setText(m2DateString);
         m1LocationLabel.setText(m1LocationString);
         m2LocationLabel.setText(m2LocationString);
+    }
+
+    //code to send the correct information to slotInformationScreens()
+    public void m1slot1Information() throws IOException {
+        slotInformationScreens(1, m1Slot1TypeString, m1Slot1ValueString);
+    }
+    public void m1slot2Information() throws IOException {
+        slotInformationScreens(2, m1Slot2TypeString, m1Slot2ValueString);
+    }
+    public void m1slot3Information() throws IOException {
+        slotInformationScreens(3, m1Slot3TypeString, m1Slot3ValueString);
+    }
+    public void m2slot1Information() throws IOException {
+        slotInformationScreens(1, m2Slot1TypeString, m2Slot1ValueString);
+    }
+    public void m2slot2Information() throws IOException {
+        slotInformationScreens(2, m2Slot2TypeString, m2Slot2ValueString);
+    }
+    public void m2slot3Information() throws IOException {
+        slotInformationScreens(3, m2Slot3TypeString, m2Slot3ValueString);
+    }
+
+    private void slotInformationScreens(int slot, String Type, String Value) throws IOException {
+        MeasureInfoScreenController.slotNumber = slot;
+        MeasureInfoScreenController.sensorValue = Value;
+        MeasureInfoScreenController.sensorType = Type;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MeasureInfoScreen.fxml"));
+        Parent root= fxmlLoader.load();
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+        stage.setTitle("Measurement Information");
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("logo.png"))));
+        String css = null;
+        if(SaveFile.theme == 0) {
+            css = Objects.requireNonNull(this.getClass().getResource("PopupMenuLight.css")).toExternalForm();
+        } else if (SaveFile.theme == 1) {
+            css = Objects.requireNonNull(this.getClass().getResource("PopupMenuDark.css")).toExternalForm();
+        }
+        scene.getStylesheets().add(css);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void backToOpenMeasurementsScreen(){
+        try {
+            AnchorPane pane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("OpenMeasurementsScreen.fxml")));
+            fxmlpane.getChildren().setAll(pane);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
